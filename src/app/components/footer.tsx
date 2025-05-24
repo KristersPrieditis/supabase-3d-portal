249096697fe887d3a38e769dca34086d20a9f739
sabrinagergish <sabrinagergish@gmail.com>
2025-05-25 01:37:34 +0200

    footer file in component folder. tailwind.config.js file added. postcss.config.js file added

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+// components/Footer.tsx
+export default function Footer() {
+    return (
+        <footer className="bg-red-500 text-white py-4 text-center">
+
+        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
